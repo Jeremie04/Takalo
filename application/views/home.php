a8054581271e9256e44d5bd4b7ba64cef2d9ad89
AnjaArisoa <arisoaanja@gmail.com>
2023-02-08 04:07:04 -0500

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -79,27 +79,34 @@
       <div class="col-lg-9">
         <br> <br>
         <div class="row">
+        <?php for($i=0;$i<6;$i++){?>
           <div class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="../assets/img/niketech.jpg" alt=""></a>
+              
+                <?php $ran=rand(0,count($one));?>
+              <img class="card-img-top" src="../assets/img/<?php echo $one[$ran]['nom']?>" alt="">
               <div class="card-body">
                 <h4 class="card-title">
-                  <a href="#">Item One</a>
+                  <a href="<?php echo site_url('Affiche/details');?>?id=<?php echo $one[$ran]['idObjet']?>"><?php echo $one[$ran]['titre'];?></a>
                 </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                <h5><?php ?><?php echo $one[$ran]['prix'];?>Ar</h5>
+                <p class="card-text"><?php echo$one[$ran]['description'];?></p>
               </div>
+              
               <div class="card-footer">
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
               </div>
             </div>
+            
           </div>
+          <?php } ?>
 
-          <div class="col-lg-4 col-md-6 mb-4">
+          <!-- <div class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
               <a href="#"><img class="card-img-top" src="../assets/img/jb.jpg" alt=""></a>
               <div class="card-body">
                 <h4 class="card-title">
+
                   <a href="#">Item Two</a>
                 </h4>
                 <h5>$24.99</h5>
@@ -109,9 +116,9 @@
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
               </div>
             </div>
-          </div>
+          </div> -->
 
-          <div class="col-lg-4 col-md-6 mb-4">
+          <!-- <div class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
               <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
               <div class="card-body">
@@ -125,9 +132,9 @@
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
               </div>
             </div>
-          </div>
+          </div> -->
 
-          <div class="col-lg-4 col-md-6 mb-4">
+          <!-- <div class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
               <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
               <div class="card-body">
@@ -173,7 +180,7 @@
                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
               </div>
             </div>
-          </div>
+          </div> -->
 
         </div>
         <!-- /.row -->
